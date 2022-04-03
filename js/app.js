b2dc2d51e28ce1bc39fcffd854c8e78bceb455a4
NaFis Fuad Mim <nafisnfm@gmail.com>
2022-04-03 19:20:26 +0600

    final

--- js/app.js
+++ js/app.js
@@ -11,3 +11,30 @@ function getPin() {
 function generatePin() {
   document.getElementById("display-pin").value = getPin();
 }
+//
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const typedNumber = document.getElementById("typed-numbers");
+  const number = event.target.innerText;
+  if (isNaN(number)) {
+    if (number == "C") {
+      typedNumber.value = "";
+    }
+  } else {
+    const previousNumber = typedNumber.value;
+    const newNumber = previousNumber + number;
+    typedNumber.value = newNumber;
+  }
+});
+function pinVerify() {
+  const generateInput = document.getElementById("display-pin").value;
+  const calcInput = document.getElementById("typed-numbers").value;
+  const wrongType = document.getElementById("pin-wrong");
+  const rightType = document.getElementById("pin-right");
+  if (generateInput == calcInput) {
+    rightType.style.display = "block";
+    wrongType.style.display = "none";
+  } else {
+    wrongType.style.display = "block";
+    rightType.style.display = "none";
+  }
+}
